65a293f92da45bee8dc29ec4eca6c36e1c2ffd04
bysaniDurga20 <bysanidurga@outlook.com>
2021-01-30 19:47:04 +0530

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,37 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouterModule } from "@angular/router";
+import { ReactiveFormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { TopBarComponent } from './top-bar/top-bar.component';
-import { ProductListComponent } from './product-list/product-list.component';
-import { ProductAlertscomponentComponent } from './product-alertscomponent/product-alertscomponent.component';
+import { AppComponent } from "./app.component";
+import { TopBarComponent } from "./top-bar/top-bar.component";
+import { ProductListComponent } from "./product-list/product-list.component";
+import { ProductAlertscomponentComponent } from "./product-alertscomponent/product-alertscomponent.component";
+import { ProductDetailsComponent } from "./product-details/product-details.component";
 
 @NgModule({
   imports: [
     BrowserModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
-      { path: '', component: ProductListComponent },
+      {
+        path: "",
+        component: ProductListComponent,
+        path: "products/:productId",
+        component: ProductDetailsComponent
+      }
     ])
   ],
   declarations: [
     AppComponent,
     TopBarComponent,
     ProductListComponent,
-    ProductAlertscomponentComponent
+    ProductAlertscomponentComponent,
+    ProductDetailsComponent
   ],
-  bootstrap: [
-    AppComponent
-  ]
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
-
+export class AppModule {}
 
 /*
 Copyright Google LLC. All Rights Reserved.
