7486671724b33d7bbd5975712360714a6bd192a5
bysaniDurga20 <bysanidurga@outlook.com>
2021-01-31 14:32:05 +0530

    2 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { AppComponent } from "./app.component";
 import { TopBarComponent } from "./top-bar/top-bar.component";
 import { ProductListComponent } from "./product-list/product-list.component";
 import { ProductAlertscomponentComponent } from "./product-alertscomponent/product-alertscomponent.component";
-import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ProductDetailsComponent } from "./product-details/product-details.component";
 
 @NgModule({
   imports: [
@@ -15,12 +15,12 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
     ReactiveFormsModule,
     RouterModule.forRoot([
       {
-        path: '',
+        path: "",
         component: ProductListComponent
       },
       {
-        path:'./products:productId',
-        component:ProductDetailsComponent
+        path: "products/:ProductId",
+        component: ProductDetailsComponent
       }
     ])
   ],
